714d4f6142ab9d33675d9320169248dd698e81e4
Marlizel Howard <marlizelh@gmail.com>
2025-08-17 09:32:49 +0200

    Update to database for online bookstore

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,44 +1,44 @@
 -- Create the database
-CREATE DATABASE alx_book_store;
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
--- Switch to the database
+-- Select the database
 USE alx_book_store;
 
--- Authors table
+-- Create Authors table
 CREATE TABLE Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215)
 );
 
--- Books table
+-- Create Books table
 CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130),
     author_id INT,
     price DOUBLE,
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Customers table
+-- Create Customers table
 CREATE TABLE Customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE,
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
 
--- Orders table
+-- Create Orders table
 CREATE TABLE Orders (
-    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Order_Details table
+-- Create Order_Details table
 CREATE TABLE Order_Details (
-    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
